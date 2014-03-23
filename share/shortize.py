93c6f94cb93e6b52ab4e631541d6fea4cf2b7358
Pierre Etchemaite <pe-pol@concept-micro.com>
2014-03-23 15:47:31 +0100

    Wine 1.4 DOSBox Linux x86 package 0.5

--- share/shortize.py
+++ share/shortize.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python
-import sys, string
+import sys, os
 
 tmp = sys.argv[1]
 tmp = tmp.replace(" ","")
-tmp = string.split(tmp,"\\")
+tmp = tmp.split("\\")
 
 i = 0
 for content in tmp:
-	content2 = string.split(content,".")
+	content2 = content.split(".")
 	if(len(content2[0]) > 8):
 		content2[0] = content2[0][0:6]+"~1" ## Fixme, we should consider the case if there are several files
-		content = string.join(content2,".")
+		content = ".".join(content2)
 	tmp[i] = content
 	i+=1
 	
-tmp = string.join(tmp,"\\")
+tmp = "\\".join(tmp)
 tmp = tmp.upper()
-print tmp
+sys.stdout.write("%s\n" % tmp)
